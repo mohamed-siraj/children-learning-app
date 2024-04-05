bb21e76e382ba11bfa8ea52ca7b68fad636509a6
Mohamed Siraj <siraj@oganro.com>
2024-04-05 20:22:59 +0530

    changes update

--- src/screens/addNew.screen.tsx
+++ src/screens/addNew.screen.tsx
@@ -1,67 +1,129 @@
-import React from 'react';
-import { Image, TouchableOpacity, Text, View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity, Text, View, Pressable, TextInput, Alert, ActivityIndicator } from 'react-native';
 import { useFonts, Poppins_900Black } from '@expo-google-fonts/poppins';
 import { ScaledSheet } from 'react-native-size-matters';
+import { auth } from '../services/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { COLORS } from '../../constant';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+
+/**
+ * store
+ */
+import { useAppDispatch } from '../store/hooks/storeTypeHook.hooks';
+import { setToken } from '../store/slices/auth.slice';
+/**
+ * Schema
+ */
+import LoginSchema from '../shema/login.shema';
 
 /**
  * 
  * type check
  */
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { TStartScreen } from '../_type/navigate.type';
+import { TLoginRegister } from '../_type/navigate.type';
+import { TLogin } from '../_type/form.type';
 
-type Props = NativeStackScreenProps<TStartScreen, 'Start'>;
+type Props = NativeStackScreenProps<TLoginRegister, "Sign In">;
 
 const AddNewScreen: React.FunctionComponent<any> = ({ navigation }: Props) => {
+  const dispatch = useAppDispatch();
+  /**
+ * local state
+ */
+  const [loading, setLoading] = useState<boolean>(false);
+
+
+  /**
+ * form initialize
+ */
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(LoginSchema),
+  });
+
+  /**
+ * form submit
+ */
+  const onSubmit: SubmitHandler<TLogin> = async (data) => {
+
+    if (!loading) {
+      setLoading(true);
+      try {
+        const result = await signInWithEmailAndPassword(auth, data.email, data.password);
+        const stringResult = JSON.stringify(result);
+        dispatch(setToken(stringResult));
+        setLoading(false);
+        navigation.navigate('Home');
+
+      } catch (error: any) {
+        Alert.alert(
+          `${error.message}`,
+        );
+        setLoading(false);
+      }
+
+    }
+  };
 
   /**
    * font load
    */
   let [fontsLoaded] = useFonts({
-    Poppins_900Black,
+    Poppins_900Black
   });
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.containerWrap}>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/family.png')} />
-            <Text style={styles.title}>Family</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/lion.png')} />
-            <Text style={styles.title}>Animals</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/bird.png')} />
-            <Text style={styles.title}>Birds</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/tamil.png')} />
-            <Text style={styles.title}>Alphabets</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/bird.png')} />
-            <Text style={styles.title}>Birds</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/tamil.png')} />
-            <Text style={styles.title}>Alphabets</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/bird.png')} />
-            <Text style={styles.title}>Birds</Text>
-          </View>
-          <View style={styles.containerRow}>
-            <Image style={styles.image} source={require('../img/tamil.png')} />
-            <Text style={styles.title}>Alphabets</Text>
-          </View>
-        </View>
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.signInText}>Sign In</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Email Address"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text style={styles.envalidText}>{errors.email.message}</Text>}
 
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            secureTextEntry={true}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text style={styles.envalidText}>{errors.password.message}</Text>}
+      <Pressable style={styles.btnLogin} onPress={handleSubmit(onSubmit)}>
+        {
+          loading ? <ActivityIndicator size="small" color={COLORS.white} /> : <Text style={styles.btnText}>Login</Text>
+        }
+      </Pressable>
+      <TouchableOpacity style={styles.createNewAc} onPress={() => {
+        navigation.navigate('Registration');
+      }}>
+        <Text>Create New Account</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -72,29 +134,52 @@ const styles = ScaledSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    justifyContent: 'center',
     backgroundColor: COLORS.primaryColor
   },
-  containerWrap: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '40@vs'
-  },
-  containerRow: {
-    backgroundColor: COLORS.white,
-    padding: '20@s',
-    borderRadius: 30,
-    margin: '10@s',
-  },
-  image: {
-    width: '100@vs',
-    height: '100@s',
-  },
-  title: {
+  signInText: {
     textAlign: 'center',
     fontFamily: 'Poppins_900Black',
-    marginTop: '10@s'
+    fontSize: '25@s',
+    color: COLORS.meroon
+  },
+  btnLogin: {
+    textAlign: 'center',
+    backgroundColor: COLORS.black,
+    marginLeft: '60@s',
+    marginRight: '60@s',
+    marginBottom: '10@vs',
+    marginTop: '10@vs',
+    borderRadius: 30,
+    height: '60@vs',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: COLORS.white,
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+  },
+  input: {
+    height: '60@vs',
+    marginLeft: '30@s',
+    marginRight: '30@s',
+    marginBottom: '10@vs',
+    marginTop: '10@vs',
+    borderWidth: 2,
+    padding: '10@vs',
+    paddingLeft: '30@vs',
+    borderRadius: 30,
+    borderColor: COLORS.meroon
+  },
+  createNewAc: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  envalidText: {
+    color: COLORS.red,
+    textAlign: 'center'
   }
-
 });
