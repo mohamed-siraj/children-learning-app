672050b50fd8b300ad839ead8f23d46e31b1a7f8
Mohamed Siraj <siraj@oganro.com>
2024-04-06 12:30:05 +0530

    changes update

--- src/services/audio.service.tsx
+++ src/services/audio.service.tsx
@@ -3,7 +3,7 @@ import { Audio } from 'expo-av';
 
 export const pronounsAudio = async (text : string) => {
     const sound = new Audio.Sound()
-    await sound.loadAsync({ uri: `http://api.voicerss.org/?key=837864db88494a459b45b3b2fde96a06&hl=en-us&c=MP3&src=${text}` });
+    await sound.loadAsync({ uri: `http://api.voicerss.org/?key=837864db88494a459b45b3b2fde96a06&r=-5&hl=ta-in&c=MP3&src=${text}` });
 
     await sound.playAsync();
 };
